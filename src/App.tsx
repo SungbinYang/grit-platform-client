30167e952f0ce375b67011394779cc284e4221c3
SungbinYang <ysb05222@naver.com>
2026-01-04 12:55:19 +0900

    chore: 클래스 네임에는 중괄호가 굳이 필요 없다

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import type { FC } from "react";
+import type { FC } from 'react';
 
 const App: FC = () => {
   return (
-    <div
-      className={"flex w-1/2 justify-center rounded-lg border-2 border-red-500"}
-    >
+    <div className="flex w-1/2 justify-center rounded-lg border-2 border-red-500">
       Hello World!
     </div>
   );
